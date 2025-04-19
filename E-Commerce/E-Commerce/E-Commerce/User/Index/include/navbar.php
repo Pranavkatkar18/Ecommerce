2c7838d440ee5ac65f9b1eff7503a6fa05c0df18
Pranav Katkar <114013056+Pranavkatkar18@users.noreply.github.com>
2025-04-19 23:32:42 +0530

    Add files via upload

---
+++ E-Commerce/E-Commerce/E-Commerce/User/Index/include/navbar.php
@@ -0,0 +1,174 @@
+<nav class="navbar navbar-expand-lg bg-light navbar-light navbar-custom sticky-top p-2">
+
+<a class="navbar-brand" href="/E-Commerce/index.html">
+
+  <img src="/E-Commerce/User/Index/img/logo.png" height="40px" width="60px">
+
+</a>
+
+<a class="navbar-brand ml-2" href="#">
+  <img src="/E-Commerce/User/Index/img/user.jpg" height="40px" width="40px" id="User" style="border-radius: 30px;">
+  <a href="/E-Commerce/index.html"><i class="fas fa-sign-out-alt" style="color: grey
+      ;"></i></a>
+</a>
+
+<button class="navbar-toggler ml-auto custom-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+
+  <ul class="navbar-nav">
+
+
+    <li class="nav-item">
+      <a class="nav-link" href="" >Women</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">Men</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">Kids</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">Tech</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">All
+        Brands</a>
+    </li>
+
+
+
+    <li class="nav-item dropdown megamenu"><a id="megamneu" href="" data-toggle="dropdown" aria-haspopup="true"
+        aria-expanded="false" class="nav-link dropdown-toggle font-weight-bold">More</a>
+      <div aria-labelledby="megamneu" class="dropdown-menu border-0 p-0 m-0">
+        <div class="container">
+          <div class="row bg-white rounded-0 m-0 shadow-sm">
+            <div class="col-lg-7 col-xl-8">
+              <div class="p-4">
+                <div class="row">
+                  <div class="col-lg-6 mb-4">
+                    <h6 class="font-weight-bold text-success">Home Essentials</h6>
+                    <ul class="list-unstyled">
+                      <li class=""
+                           class="nav-link text-small pb-0">Decor</a>
+                      </li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Kitchenware</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Bedding</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Appliances</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-6 mb-4">
+                    <h6 class="font-weight-bold text-success">Footwear</h6>
+                    <ul class="list-unstyled">
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Heels</a>
+                      </li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Wedges</a>
+                      </li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Flipflops</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Sports
+                          Shoes</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-6 mb-4">
+                    <h6 class="font-weight-bold text-success">Jewellery</h6>
+                    <ul class="list-unstyled">
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Fashion
+                          Jewellery</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Fine
+                          Jewellery</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Silver
+                          Jewellery</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Jewellery
+                          Sets</a></li>
+                    </ul>
+                  </div>
+                  <div class="col-lg-6 mb-4">
+                    <h6 class="font-weight-bold text-success">FitZone</h6>
+                    <ul class="list-unstyled">
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Fitness
+                          Accessories</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Gadgests
+                          On The Go</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Equipments</a></li>
+                      <li class="nav-item"><a
+                          href=""
+                          class="nav-link text-small pb-0 ">Activewear</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 p-3">
+            <img src="https://images.unsplash.com/photo-1513094735237-8f2714d57c13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNob3BwaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=60" height="100%" width="100%">
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </li>
+    <li class="nav-item">
+      <form class="form-inline" action="/Login_Ecommerce/User/index.html" target="_blank">
+
+        <input class="form-control ml-sm-2 ml-3" type="text" placeholder="Search for products, styles, brands"
+          style="width: 260px;">
+
+
+
+      </form>
+    </li>
+
+    <li class="nav-bitem">
+      <a href= "/E-Commerce/User/Index/viewcart.php" class= "btn btn-primary mb-1" style="height: 40px; width: 80px;">Cart </a>
+      
+    </li>
+
+    <li class="nav-bitem">
+      <a href= "/E-Commerce/User/Index/Sql/clrcart.php" class= "btn btn-danger mb-1" id="nav-btn"style="height: 40px; width: 110px;">Clear Cart </a>
+      
+    </li>
+
+   
+
+
+
+  </ul>
+</div>
+
+
+</nav>
+
+
+
